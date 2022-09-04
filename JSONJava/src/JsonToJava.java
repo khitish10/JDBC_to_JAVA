c16b830cff9abfff56d59368ac245dc81332faff
khitish <khitish10@gmail.com>
2022-09-04 17:43:10 +0530

    Storing multiple data into an array list

--- JSONJava/src/JsonToJava.java
+++ JSONJava/src/JsonToJava.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 import com.fasterxml.jackson.core.exc.StreamWriteException;
 import com.fasterxml.jackson.databind.DatabindException;
@@ -18,25 +19,30 @@ public class JsonToJava {
 		Connection conn=null;
 		conn = DriverManager.getConnection("jdbc:mysql://localhost:3307/Business","root","Asdf1234");//connecting to Mysql
 		
-		//Create  object statement class to execute queries
-		Statement st = conn.createStatement();
-		ResultSet rs = st.executeQuery("select * from business.customerinfo where Location='Asia' limit 1;");//holds the query result
-//		rs.next();//setting the pointer for to 1st row
-//		rs.getString(1);//1st column is string type
-//		rs.getString(2);//2nd column is String type
-//		rs.getInt(3);//3rd column is Int type
-//		rs.getString(4);//4th column 
-		CustomerDetails c = new CustomerDetails();//create object to pass value to setter methods
+		Statement st = conn.createStatement();//Create  object statement class to execute queries
+		ResultSet rs = st.executeQuery("select * from business.customerinfo where Location='Asia';");//holds the query result
+		
+		ArrayList<CustomerDetails> a = new ArrayList<>();//Create array list to store multiple customer details inside each objects
+		
 		while(rs.next()) {//moves the pointer to end of 1st row and then prints all columns in that row
+			CustomerDetails c = new CustomerDetails();//create object to pass value to setter methods
 			c.setCoureseName(rs.getString(1));
 			c.setPurchasedDate(rs.getString(2));
 			c.setAmount(rs.getInt(3));
 			c.setLocation(rs.getString(4));
-			
+			a.add(c);//Store objects of customer details into arraylist
 		}
 		
-		ObjectMapper o = new ObjectMapper();
-		o.writeValue(new File("C:\\Users\\khitish\\git\\repository3\\JSONJava\\customerInfo.json"), c);//create a new file and put the content
+		//create a loop to access each arraylist object and convert them to json
+		for(int i=0;i<a.size();i++) {
+			ObjectMapper o = new ObjectMapper();
+			o.writeValue(new File("C:\\Users\\khitish\\git\\repository3\\JSONJava\\customerInfo"+i+".json"), a.get(i));//create a new file and put the content
+		}
+		
+		
+		
+		
+		
 		conn.close();
 	}
 
