62f1a56a8b36183a8ee3dea3c80587855f941610
khitish <khitish10@gmail.com>
2022-09-04 17:19:14 +0530

    Add Jackson dependency.Final JSON file for 1 customer detail

--- JSONJava/src/JsonToJava.java
+++ JSONJava/src/JsonToJava.java
@@ -1,12 +1,18 @@
+import java.io.File;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import com.fasterxml.jackson.core.exc.StreamWriteException;
+import com.fasterxml.jackson.databind.DatabindException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class JsonToJava {
 	
-	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException, StreamWriteException, DatabindException, IOException {
 		
 		Class.forName("com.mysql.cj.jdbc.Driver");//this class is responsible for all JDBC methods and help us to talk to db
 		Connection conn=null;
@@ -20,18 +26,17 @@ public class JsonToJava {
 //		rs.getString(2);//2nd column is String type
 //		rs.getInt(3);//3rd column is Int type
 //		rs.getString(4);//4th column 
+		CustomerDetails c = new CustomerDetails();//create object to pass value to setter methods
 		while(rs.next()) {//moves the pointer to end of 1st row and then prints all columns in that row
-			CustomerDetails c = new CustomerDetails();//create object to passs value to setter methods
 			c.setCoureseName(rs.getString(1));
 			c.setPurchasedDate(rs.getString(2));
 			c.setAmount(rs.getInt(3));
 			c.setLocation(rs.getString(4));
 			
-			System.out.println(c.getCoureseName());
-			System.out.println(c.getPurchasedDate());
-			System.out.println(c.getAmount());
-			System.out.println(c.getLocation());
 		}
+		
+		ObjectMapper o = new ObjectMapper();
+		o.writeValue(new File("C:\\Users\\khitish\\git\\repository3\\JSONJava\\customerInfo.json"), c);//create a new file and put the content
 		conn.close();
 	}
 
