a3736e5481b886be55d0729c856d20ce226f33be
khitish <khitish10@gmail.com>
2022-09-04 16:51:08 +0530

    Create a new class to define getters and setters(POJO implementation).

---
+++ JSONJava/src/CustomerDetails.java
@@ -0,0 +1,34 @@
+
+public class CustomerDetails {
+	
+	private String coureseName;
+	private String purchasedDate;
+	private int amount;
+	private String location;
+	
+	public String getCoureseName() {
+		return coureseName;
+	}
+	public void setCoureseName(String coureseName) {
+		this.coureseName = coureseName;
+	}
+	public String getPurchasedDate() {
+		return purchasedDate;
+	}
+	public void setPurchasedDate(String purchasedDate) {
+		this.purchasedDate = purchasedDate;
+	}
+	public int getAmount() {
+		return amount;
+	}
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	
+}
--- JSONJava/src/JsonToJava.java
+++ JSONJava/src/JsonToJava.java
@@ -14,17 +14,23 @@ public class JsonToJava {
 		
 		//Create  object statement class to execute queries
 		Statement st = conn.createStatement();
-		ResultSet rs = st.executeQuery("select * from business.customerinfo;");//holds the query result
+		ResultSet rs = st.executeQuery("select * from business.customerinfo where Location='Asia' limit 1;");//holds the query result
 //		rs.next();//setting the pointer for to 1st row
 //		rs.getString(1);//1st column is string type
 //		rs.getString(2);//2nd column is String type
 //		rs.getInt(3);//3rd column is Int type
 //		rs.getString(4);//4th column 
 		while(rs.next()) {//moves the pointer to end of 1st row and then prints all columns in that row
-			System.out.println(rs.getString(1));
-			System.out.println(rs.getString(2));
-			System.out.println(rs.getInt(3));
-			System.out.println(rs.getString(4));
+			CustomerDetails c = new CustomerDetails();//create object to passs value to setter methods
+			c.setCoureseName(rs.getString(1));
+			c.setPurchasedDate(rs.getString(2));
+			c.setAmount(rs.getInt(3));
+			c.setLocation(rs.getString(4));
+			
+			System.out.println(c.getCoureseName());
+			System.out.println(c.getPurchasedDate());
+			System.out.println(c.getAmount());
+			System.out.println(c.getLocation());
 		}
 		conn.close();
 	}
